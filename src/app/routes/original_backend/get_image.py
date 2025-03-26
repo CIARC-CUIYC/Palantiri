4bb9b63d1b4b9a56705de7ea450a7ab8657dfff3
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2025-03-26 03:14:02 +0100

    finishes get image endpoint

--- src/app/routes/original_backend/get_image.py
+++ src/app/routes/original_backend/get_image.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, request, send_file
+from flask import Blueprint, send_file
+from src.app.image_loader import get_map_chunk
 from src.app.models.melvin import melvin
-from werkzeug.exceptions import BadRequest
-from PIL import Image
 import io
 
 bp = Blueprint('image', __name__, url_prefix='/image')
@@ -14,10 +13,7 @@ def get_image():
         angle = melvin.camera_angle
         # TODO: implement image access
         img = get_map_chunk(melvin_pos, angle.get_side_length())
-        img_byte_array = io.BytesIO()
-        img.save(img_byte_array, format='PNG')
-        img_byte_array.seek(0)
-
-        return send_file(img_byte_array, mimetype='image/png')
+        img_stream = io.BytesIO(img)
+        return send_file(img_stream, mimetype='image/png')
     except Exception as e:
         return {"error": f"An error occurred: {str(e)}"}, 500
