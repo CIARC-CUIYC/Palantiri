3daba979e7351cd4740c87d1360535cb42653e6f
NicolaiKallis <Nicolai.Kallis@gmx.de>
2025-03-26 06:25:44 +0100

    documents expected JSON body for the add_objectives method

--- src/app/routes/original_backend/objective.py
+++ src/app/routes/original_backend/objective.py
@@ -11,7 +11,17 @@ def objective():
     return jsonify(obj_manager.get_all_objectives())
 
 
-# TODO: Document the required formatting for the PUT command
+"""
+The expected JSON body for this endpoint is:
+{
+  "num_random_zoned": 0,
+  "num_random_beacon": 0,
+  "zoned_objectives": [],    // optional
+  "beacon_objectives": []     // optional
+}
+"""
+
+
 @bp.route('/', methods=['PUT'])
 def add_objectives():
     data = request.get_json()
